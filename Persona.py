58b1dffecd7581e754614dbda8e72abc77cbec5d
JhoanDavid28 <pjhoandavid@outlook.com>
2024-05-16 22:01:54 -0500

    Agregado Encapsulamiento para todos los atributos de la clase persona

--- Persona.py
+++ Persona.py
@@ -2,9 +2,10 @@ class Persona:
     # atributos o características de una clase
     def __init__(self, nombre, apellido, edad): # *args: tupla de argumentos variables, **kwargs: para pasar un diccionario de valores
         # atributos de instancia
-        self._nombre = nombre # el guion bajo se conoce como encapsulamiento
-        self.apellido = apellido
-        self.edad = edad
+        # el guion bajo se conoce como ENCAPSULAMIENTO
+        self._nombre = nombre 
+        self._apellido = apellido
+        self._edad = edad
 
     # Decorador para modificar el comportamiento de nuestro método
     @property # el método "nombre" va a recuperar la informacion del atributo "_nombre"
@@ -19,21 +20,34 @@ class Persona:
         print('Llamando método set nombre')
         self._nombre = nombre # ahora el se puede modificar los valores del atributo 
 
-  
+    # GET para la propiedad apellido
+    @property
+    def apellido(self):
+        return self._apellido
+    
+    # SET para la propiedad apellido
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+    
+    @property
+    def edad(self):
+        return self._edad
+    
+    @edad.setter
+    def edad(self, edad):
+        self._edad = edad
+
     # Metódo o funciones de una clase
     def mostrar_detalle(self):
-        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
+        print(f'Persona: {self._nombre} {self._apellido} {self._edad}')
 
 
 persona1 = Persona ('Juan', 'Perez', 28 )
 
-#print(f'Objeto Persona 2: {persona1.nombre}, {persona1.apellido}, {persona1.edad}')
-#persona1.mostrar_detalle()  
-#print(persona1.nombre)
-# agregando el nuevo valor mediante el método set, se encapsula el atributo _nombre
 persona1.nombre = 'Juan Carlos'
-print(persona1.nombre)
+persona1.apellido = 'Lara'
+persona1.edad = 30
+persona1.mostrar_detalle()
 
-#persona1.telefono = '555442211'
-#print(persona1.telefono)
 
