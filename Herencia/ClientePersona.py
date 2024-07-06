cace09262c13c2ba3b9d24b4c49b0bcb2a9af2a1
JhoanDavid28 <pjhoandavid@outlook.com>
2024-07-05 23:49:57 -0500

    Agregado ejercicio Sobreescritura del método __str__()

---
+++ Herencia/ClientePersona.py
@@ -0,0 +1,7 @@
+from Persona import *
+
+persona1 = Persona("Juan", 28)
+print(persona1)
+
+empleado1 = Empleado("Rose", 34, 2000)
+print(empleado1)
--- Herencia/Persona.py
+++ Herencia/Persona.py
@@ -5,12 +5,21 @@ class Persona:
         self.nombre = nombre
         self.edad = edad
 
+    # metodo str sirve para sobreescribir
+    def __str__(self):
+        # muestramos el estado de la clase
+        return f'Persona: [Nombre: {self.nombre}, Edad: {self.edad}]'        
+
 # Clase Hija
 class Empleado(Persona):
     def __init__(self, nombre, edad, sueldo):
         # metodo super sirve para acceder a los metodos de la clase padre
         super().__init__(nombre, edad)
         self.sueldo = sueldo
+        
+    def __str__(self):
+        return f"Empleado: [Sueldo: {self.sueldo}] {super().__str__()} "
 
-empleado1 = Empleado("Pablo", 28, 5000)
-print(empleado1.nombre)
+
+#empleado1 = Empleado("Pablo", 28, 5000)
+#print(empleado1.nombre)
